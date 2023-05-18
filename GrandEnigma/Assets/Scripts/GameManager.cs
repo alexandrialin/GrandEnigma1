f33a24e9fb2897de38b0cb527c237eab0ffe30df
Alexandria <alin92@uwo.ca>
2023-05-18 14:45:30 -0400

    Update GameManager.cs

--- GrandEnigma/Assets/Scripts/GameManager.cs
+++ GrandEnigma/Assets/Scripts/GameManager.cs
@@ -15,20 +15,26 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-       
         print("player connected");
-        GameObject obj = PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector3(this.transform.position.x , this.transform.position.y, this.transform.position.z), Quaternion.identity, 0);
-        
-        //obj.transform.SetParent(PlayerGrid.transform, false);
-        photonView.RPC("ParentPlayer", RpcTarget.All, obj);
-
-       
-    } 
-    
-    [PunRPC] void ParentPlayer(GameObject yees)
-    {
-        yees.transform.SetParent(PlayerGrid.transform, false);
+        GameObject obj = PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity, 0);
+        photonView.RPC("ParentPlayer", RpcTarget.All, obj.GetComponent<PhotonView>().ViewID);
     }
+
+
+    [PunRPC]
+    void ParentPlayer(int viewID)
+    {
+        PhotonView targetView = PhotonView.Find(viewID);
+        if (targetView != null)
+        {
+            targetView.transform.SetParent(PlayerGrid.transform, false);
+        }
+        else
+        {
+            Debug.LogError("Failed to find target PhotonView with ViewID: " + viewID);
+        }
+    }
+
 
 
     public void OnPhotonPlayerConnected(Player player)
